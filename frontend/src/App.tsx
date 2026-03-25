19663306ac9e84d25edfa2ce482f795e9b82f397
Abiney01 <abineynew@gmail.com>
2026-03-25 21:59:27 +0530

    initialized frontend files and folders

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-cyan-400 underline border-2 p-4 flex justify-center text-2xl font-medium text-white">App</div>
+  )
+}
+
+export default App
